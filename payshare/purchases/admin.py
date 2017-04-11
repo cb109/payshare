998879cc633d561473386673352e5684a12a9951
Christoph Buelter <c.buelter@arcor.de>
2017-04-11 22:26:31 +0200

    Add basic models and expose them via admin.

--- payshare/purchases/admin.py
+++ payshare/purchases/admin.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from payshare.purchases.models import Collective
+from payshare.purchases.models import Membership
+from payshare.purchases.models import Purchase
+
 # Register your models here.
+
+admin.site.register(Collective)
+admin.site.register(Membership)
+admin.site.register(Purchase)
--- payshare/purchases/models.py
+++ payshare/purchases/models.py
@@ -2,5 +2,69 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils import timezone
 
-# Create your models here.
+from djmoney.models.fields import MoneyField
+
+
+class PayShareError(Exception):
+    pass
+
+
+class BuyerNotMemberOfCollectiveError(PayShareError):
+    pass
+
+
+class TimestampMixin(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Collective(TimestampMixin, models.Model):
+    name = models.CharField(max_length=100)
+
+    def is_member(self, user):
+        try:
+            Membership.objects.get(collective=self, member=user)
+            return True
+        except Membership.DoesNotExist:
+            return False
+
+    def __unicode__(self):
+        return u"{}".format(self.name)
+
+
+class Membership(TimestampMixin, models.Model):
+    member = models.ForeignKey("auth.User")
+    collective = models.ForeignKey("purchases.Collective")
+
+    def __unicode__(self):
+        return u"{} in {}".format(self.member.username,
+                                  self.collective.name)
+
+
+class Purchase(TimestampMixin, models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = MoneyField(max_digits=10,
+                       decimal_places=2,
+                       default_currency='EUR')
+    buyer = models.ForeignKey("auth.User")
+    collective = models.ForeignKey("purchases.Collective")
+
+    def __unicode__(self):
+        return u"{} for {} by {} in {}".format(self.price,
+                                               self.name,
+                                               self.buyer.username,
+                                               self.collective.name)
+
+
+@receiver(pre_save, sender=Purchase)
+def purchase_pre_save_ensure_membership(sender, instance, *args, **kwargs):
+    if not instance.collective.is_member(instance.buyer):
+        raise BuyerNotMemberOfCollectiveError()
