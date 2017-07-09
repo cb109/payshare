d3e8c73abd8d0c25e11567b11122214c992e8b11
Christoph Buelter <c.buelter@arcor.de>
2017-07-09 20:26:32 +0200

    Add favicon.

--- payshare/settings.py
+++ payshare/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 365  # One year.
