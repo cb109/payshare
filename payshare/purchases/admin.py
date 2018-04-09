37d3bf5343f9d33a4867661fea3b2cc6bfbb10df
Christoph Buelter <c.buelter@arcor.de>
2018-04-09 22:45:57 +0200

    Allow easy editing of multiple users via inline form widgets in admin.

--- payshare/purchases/admin.py
+++ payshare/purchases/admin.py
@@ -2,11 +2,17 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from payshare.purchases.models import Collective
 from payshare.purchases.models import Membership
 from payshare.purchases.models import Purchase
 from payshare.purchases.models import Liquidation
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "username", "email")
+    list_editable = ("first_name", "last_name", "username", "email")
 
 
 class CollectiveAdmin(admin.ModelAdmin):
@@ -22,6 +28,9 @@ class LiquidationAdmin(admin.ModelAdmin):
     list_display = ["description", "deleted", "amount", "creditor", "debtor",
                     "id", "collective"]
 
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Collective, CollectiveAdmin)
 admin.site.register(Membership)
