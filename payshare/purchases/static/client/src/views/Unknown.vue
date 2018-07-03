9c4f2507616aba3b965f0f4f743fa1f071d5784e
Christoph Buelter <c.buelter@arcor.de>
2018-07-03 23:33:00 +0200

    Remember visited collective urls and list them on /unknown as links.

--- payshare/purchases/static/client/src/views/Unknown.vue
+++ payshare/purchases/static/client/src/views/Unknown.vue
@@ -1,11 +1,43 @@
 <template>
   <v-container fluid
                fill-height>
-    <v-layout justify-center
-              align-center>
-        <h1>
-          You seem lost. You need to open this app from an invitation link.
+    <v-layout align-center
+              column
+              class="mt-4">
+        <h1 class="headline">
+          {{ $t('unknownHint') }}
         </h1>
+        <div v-if="previousCollectiveKeys.length"
+             class="mt-4">
+          <h4 class="subheading mb-2">
+            {{ $t('beenHereBefore') }}:
+          </h4>
+          <ul>
+            <li v-for="prevKey in previousCollectiveKeys"
+                :key="prevKey">
+                <a :href="'/' + prevKey">
+                  {{ prevKey }}
+                </a>
+            </li>
+          </ul>
+        </div>
     </v-layout>
   </v-container>
 </template>
+
+
+<script>
+
+export default {
+  name: 'unknown',
+  computed: {
+    previousCollectiveKeys() {
+      return this.$store.state.previousCollectiveKeys || []
+    },
+  },
+  created() {
+    this.$store.commit('LOAD_PREVIOUS_COLLECTIVE_KEYS_FROM_LOCALSTORAGE')
+  },
+}
+
+</script>
