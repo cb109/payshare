8292f5f88fea54df0d15bf863137c787efe1df3b
Christoph Buelter <buelter.christoph@gmail.com>
2019-09-01 22:23:26 +0200

    Add middleware to reject create/modify/delete on readonly Collectives

--- payshare/purchases/middleware.py
+++ payshare/purchases/middleware.py
@@ -1,5 +1,10 @@
 import logging
 
+from django.urls.base import resolve
+
+from payshare.purchases import api
+from payshare.purchases.models import CollectiveReadOnlyError
+
 logger = logging.getLogger(__name__)
 
 
@@ -8,6 +13,29 @@ def debugging_middleware(get_response):
     def middleware(request):
         from pprint import pprint
         pprint(dict(request.META))
+
+        response = get_response(request)
+        return response
+
+    return middleware
+
+
+def readonly_middleware(get_response):
+    """Reject all requests that may change data when Collective is readonly.
+
+    This middleware must come after the AuthenticationMiddleware so we
+    can identify the Collective.
+
+    """
+    def middleware(request):
+        if request.method not in ("GET", "OPTIONS"):
+
+            resolver_match = resolve(request.path)
+            key = api.key_from_resolvermatch(resolver_match)
+            collective = api.collective_from_key(key)
+
+            if collective.readonly:
+                raise CollectiveReadOnlyError(collective)
 
         response = get_response(request)
         return response
