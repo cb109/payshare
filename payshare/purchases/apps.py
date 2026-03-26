386ce9f20ea4b83b5c0f1dec7f6ffc0d3159a63a
Christoph Buelter <buelter.christoph@gmail.com>
2026-03-26 16:05:30 +0100

    Use uv for dependency management and update dependencies to latest

--- payshare/purchases/apps.py
+++ payshare/purchases/apps.py
@@ -1,8 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
 class PurchasesConfig(AppConfig):
-    name = 'purchases'
+    name = 'payshare.purchases'
