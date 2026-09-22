881e0548b481e245cf8672f9586ad2a8784cf541
Christoph Buelter <buelter.christoph@gmail.com>
2026-09-22 17:25:12 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-workon payshare &&
-
-pip install pip-tools &&
-pip-sync &&
+uv sync &&
 
 cd payshare/purchases/static/client &&
 yarn install --check-files &&
 npm run build &&
 
 cd - &&
-python manage.py collectstatic --noinput
+uv run manage.py collectstatic --noinput
 
 echo "build finished"
